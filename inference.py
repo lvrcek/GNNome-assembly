5a21da1642639811e531c0bf119a88ff09129cc1
Lovro Vrcek <vrcek.lovro@gmail.com>
2022-01-24 16:52:25 +0800

    Martin's code for N50 and NG50

--- inference.py
+++ inference.py
@@ -74,20 +74,81 @@ def decode(neighbors, edges, start, logits):
     return walk
 
 
-def analyze(graph, gnn_paths, greedy_paths, out):
+def calculate_N50(list_of_lengths):
+    """Calculate N50 for a sequence of numbers.
+    Args:
+        list_of_lengths (list): List of numbers.
+    Returns:
+        float: N50 value.
+    """
+    tmp = []
+    for tmp_number in set(list_of_lengths):
+        tmp += [tmp_number] * list_of_lengths.count(tmp_number) * tmp_number
+    tmp.sort()
+
+    if (len(tmp) % 2) == 0:
+        median = (tmp[int(len(tmp) / 2) - 1] + tmp[int(len(tmp) / 2)]) / 2
+    else:
+        median = tmp[int(len(tmp) / 2)]
+
+    return median
+
+def calculate_NG50(list_of_lengths, ref_length):
+    """Calculate N50 for a sequence of numbers.
+    Args:
+        list_of_lengths (list): List of numbers.
+    Returns:
+        float: N50 value.
+    """
+    if ref_length == 0:
+        return -1
+    list_of_lengths.sort(reverse=True)
+    total_bps = 0
+    for contig in list_of_lengths:
+        total_bps += contig
+        if total_bps > ref_length/2:
+            return contig
+    return -1
+
+def txt_output(f, txt):
+    print(f'\t{txt}')
+    f.write(f'\t{txt}\n')
+
+def analyze(graph, gnn_paths, greedy_paths, out, ref_length):
     with open(f'{out}/analysis.txt', 'w') as f:
         # f.write(f'Chromosome total length:\t\n')
-        print(f'GNN contigs:\t{len(gnn_paths)}\n')
-        f.write(f'GNN contigs:\t{len(gnn_paths)}\n')
+        #print(out.split("/"), out.split("/")[-2])
+        gnn_contig_lengths = []
         for path in gnn_paths:
-            print(f'\t{graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]}\n')
-            f.write(f'\t{graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]}\n')
-        print(f'Greedy paths:\t{len(greedy_paths)}\n')
-        f.write(f'Greedy paths:\t{len(greedy_paths)}\n')
+            contig_len = graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]
+            gnn_contig_lengths.append(abs(contig_len).item())
+        txt_output(f, 'GNN: ')
+        txt_output(f, f'Contigs: \t{gnn_contig_lengths}')
+        txt_output(f,f'Contigs amount:\t{len(gnn_contig_lengths)}')
+        txt_output(f,f'Longest Contig:\t{max(gnn_contig_lengths)}')
+        txt_output(f,f'Reconstructed:\t{sum(gnn_contig_lengths)}')
+        txt_output(f,f'Percentage:\t{sum(gnn_contig_lengths)/ref_length*100}')
+        n50_gnn = calculate_N50(gnn_contig_lengths)
+        txt_output(f,f'N50:\t{n50_gnn}')
+        ng50_gnn = calculate_NG50(gnn_contig_lengths, ref_length)
+        txt_output(f,f'NG50:\t{ng50_gnn}')
+
+
+        txt_output(f,f'Greedy paths:\t{len(greedy_paths)}\n')
+        greedy_contig_lengths = []
         for path in greedy_paths:
-            print(f'\t{graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]}\n')
-            f.write(f'\t{graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]}\n')
-        # Calculate N50, NG50, NGA50
+            contig_len = graph.ndata["read_end"][path[-1]] - graph.ndata["read_start"][path[0]]
+            greedy_contig_lengths.append(abs(contig_len).item())
+        txt_output(f, 'Greedy: ')
+        txt_output(f, f'Contigs: \t{greedy_contig_lengths}')
+        txt_output(f,f'Contigs amount:\t{len(greedy_contig_lengths)}')
+        txt_output(f,f'Longest Contig:\t{max(greedy_contig_lengths)}')
+        txt_output(f,f'Reconstructed:\t{sum(greedy_contig_lengths)}')
+        txt_output(f,f'Percentage:\t{sum(greedy_contig_lengths)/ref_length*100}')
+        n50_greedy = calculate_N50(greedy_contig_lengths)
+        txt_output(f,f'N50:\t{n50_greedy}')
+        ng50_greedy = calculate_NG50(greedy_contig_lengths, ref_length)
+        txt_output(f,f'NG50:\t{ng50_greedy}')
 
 
 def inference(model_path=None, data_path=None):
