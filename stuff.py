7f24ef1e2e9cac8235cfffc50b7bf01ace7e4730
Lovro Vrcek <vrcek.lovro@gmail.com>
2022-03-02 11:00:27 +0800

    Update generating the solutions

--- stuff.py
+++ stuff.py
@@ -1,5 +1,6 @@
 import pickle
 import subprocess
+import os
 
 import dgl
 import torch
@@ -17,10 +18,15 @@ def run_new_gt_algo():
             neighs = pickle.load(open(f'{path}/chr{i}/info/0_succ.pkl', 'rb'))
             edges = pickle.load(open(f'{path}/chr{i}/info/0_edges.pkl', 'rb'))
             n_p, e_p, n_n, e_n, all_walks = algorithms.dfs_gt_graph(graph, neighs, edges)
-            line = f'chr{i},{len(all_walks)},{len(n_p)+len(n_e)},{graph.num_nodes()},\
-                    {(len(n_p)+len(n_e))/graph.num_nodes()},{len(e_p)+len(e_n)},\
+            line = f'chr{i},{len(all_walks)},{len(n_p)+len(n_n)},{graph.num_nodes()},\
+                    {(len(n_p)+len(n_n))/graph.num_nodes()},{len(e_p)+len(e_n)},\
                     {graph.num_edges()},{(len(e_p)+len(e_n))/graph.num_edges()}\n'
             f.write(line)
+            correct_nodes = n_p | n_n
+            correct_edges = e_p | e_n
+            if not os.path.isdir(f'{path}/chr{i}/solutions'):
+                pickle.dump(correct_nodes, open(f'{path}/chr{i}/solutions/0_nodes.pkl', 'wb'))
+                pickle.dump(correct_edges, open(f'{path}/chr{i}/solutions/0_edges.pkl', 'wb'))
 
 
 if __name__ == '__main__':
