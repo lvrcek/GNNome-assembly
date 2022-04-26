604c98ee0546111943afbba0543f3eef794d3521
Lovro Vrcek <vrcek.lovro@gmail.com>
2022-04-26 20:05:07 +0800

    Added read simulation with seqrequester

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,38 @@
+import gzip
 import os
 import subprocess
 
+from tqdm import tqdm
 import requests
 from Bio import SeqIO
 
+
+
+chr_lens = {
+'chr1': 248387328,
+'chr2': 242696752,
+'chr3': 201105948,
+'chr4': 193574945,
+'chr5': 182045439,
+'chr6': 172126628,
+'chr7': 160567428,
+'chr8': 146259331,
+'chr9': 150617247,
+'chr10': 134758134,
+'chr11': 135127769,
+'chr12': 133324548,
+'chr13': 113566686,
+'chr14': 101161492,
+'chr15': 99753195,
+'chr16': 96330374,
+'chr17': 84276897,
+'chr18': 80542538,
+'chr19': 61707364,
+'chr20': 66210255,
+'chr21': 45090682,
+'chr22': 51324926,
+'chrX': 154259566,
+}
 
 def create_chr_dirs(pth):
     for i in range(1, 24):
@@ -48,14 +77,29 @@ def download_reference():
     chm_path = os.path.join(ref_path, 'CHM13')
     chr_path = os.path.join(ref_path, 'chromosomes')
     chm13_url = 'https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.1.fasta.gz'
+    chm13_path = os.path.join(chm_path, 'chm13.draft_v1.1.fasta.gz')
+
     if len(os.listdir(chm_path)) == 0:
         # Download the CHM13 reference
-        response = requests.get(chm13_url)
+        # Code for tqdm from: https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
+        response = requests.get(chm13_url, stream=True)
+        total_size_in_bytes= int(response.headers.get('content-length', 0))
+        block_size = 1024 #1 Kibibyte
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+        with open(chm13_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
+
     if len(os.listdir(chr_path)) == 0:
         # Parse the CHM13 into individual chromosomes
-        chm_path = os.path.join(chm_path, 'chm13.draft_v1.1.fasta.gz')
-        for record in SeqIO.parse(chm_path, 'fasta'):
-            SeqIO.write(record, os.path.join(chr_path, f'{record.id}.fasta'), 'fasta')
+        with gzip.open(chm13_path, 'rt') as f:
+            for record in SeqIO.parse(f, 'fasta'):
+                SeqIO.write(record, os.path.join(chr_path, f'{record.id}.fasta'), 'fasta')
 
 
 # 1. Simulate the sequences
@@ -67,7 +111,7 @@ def simualate_reads(chr_dict):
     len_path = os.path.join(ref_path, 'lengths')
     sim_path = os.path.abspath('data/neurips/simulated')
     for chrN, n_need in chr_dict.items():
-        chr_raw_path = os.path.joins(f'sim_path/{chrN}/raw')
+        chr_raw_path = os.path.join(sim_path, f'{chrN}/raw')
         n_have = len(os.listdir(chr_raw_path))
         if n_need <= n_have:
             continue
@@ -75,7 +119,14 @@ def simualate_reads(chr_dict):
             n_diff = n_need - n_have
             # Simulate reads for chrN n_diff times
             # Be careful how you name them
-            ...    
+            chr_seq_path = os.path.join(chr_path, f'{chrN}.fasta')
+            chr_dist_path = os.path.join(len_path, f'{chrN}.txt')
+            chr_len = chr_lens[chrN]
+            for i in range(n_diff):
+                idx = n_have + i
+                chr_save_path = os.path.join(chr_raw_path, f'{idx}.fasta')
+                subprocess.run(f'./vendor/seqrequester/build/bin/seqrequester simulate -genome {chr_seq_path} -genomesize {chr_len} -coverage 32.4 -distribution {chr_dist_path} > {chr_save_path}', shell=True)
+
 
 
 # 2. Generate the graphs
@@ -147,4 +198,4 @@ if __name__ == '__main__':
     # Or just reads everything from some config file
     # E.g., train = {chr1: 1, chr4: 3, chr5: 5}
     # E.g., eval = {chr6: 2, chr5: 3}
-
+    pass
