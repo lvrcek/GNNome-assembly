Train data leak fixed, more verbose log messages

train.py
@@ -282,7 +282,7 @@ def train(data, out, eval, overfit):
 
                         # Run Metis
                         g = g.long()
-                        num_clusters = torch.LongTensor(1).random_(num_parts_metis_train-250,num_parts_metis_train+250).item() # DEBUG!!!
+                        num_clusters = torch.LongTensor(1).random_(num_parts_metis_train-50,num_parts_metis_train+50).item() # DEBUG!!!
                         sampler = dgl.dataloading.ClusterGCNSampler(g, num_clusters, cache_path=cluster_cache_path) 
                         dataloader = dgl.dataloading.DataLoader(g, torch.arange(num_clusters), sampler, batch_size=batch_size_train, shuffle=True, drop_last=False, num_workers=4) # XB
 
@@ -377,7 +377,7 @@ def train(data, out, eval, overfit):
                     scheduler.step(train_loss_all_graphs)
 
                 #if not overfit:
-                if not epoch%10 and epoch>0: # DEBUG !!!!!!!!!!!!!
+                if not epoch % 3 and epoch > 0: # DEBUG !!!!!!!!!!!!!
 
                     val_loss_all_graphs, val_fp_rate_all_graphs, val_fn_rate_all_graphs = [], [], []
                     val_acc_all_graphs, val_precision_all_graphs, val_recall_all_graphs, val_f1_all_graphs = [], [], [], []

utils.py
@@ -167,36 +167,36 @@ def add_positional_encoding(g, pe_dim):
     g.ndata['in_deg'] = g.in_degrees().float()
     g.ndata['out_deg'] = g.out_degrees().float()
     
-    # # Geometric diffusion features with Random Walk
-    # A = g.adjacency_matrix(scipy_fmt="csr")
-    # Dinv = sp.diags(dgl.backend.asnumpy(g.in_degrees()).clip(1) ** -1.0, dtype=float) # D^-1
-    # RW = A @ Dinv  
-    # M = RW
-    # # Iterate
-    # PE = [torch.from_numpy(M.diagonal()).float()]
-    # M_power = M
-    # for _ in range(pe_dim-1):
-    #     M_power = M_power @ M
-    #     PE.append(torch.from_numpy(M_power.diagonal()).float())
-    # PE = torch.stack(PE,dim=-1)
-    # g.ndata['pe'] = PE  
-
-    # k-step PageRank features
+    # Geometric diffusion features with Random Walk
     A = g.adjacency_matrix(scipy_fmt="csr")
-    D = A.sum(axis=1) # out degree
-    Dinv = 1./ (D+1e-9); Dinv[D<1e-9] = 0 # take care of nodes without outgoing edges
-    Dinv = sp.diags(np.squeeze(np.asarray(Dinv)), dtype=float) # D^-1 
-    P = (Dinv @ A).T 
-    n = A.shape[0]
-    One = np.ones([n])
-    x = One/ n
-    PE = [] 
-    alpha = 0.95 
-    for _ in range(pe_dim): 
-        x = alpha* P.dot(x) + (1.0-alpha)/n* One 
-        PE.append(torch.from_numpy(x).float())
+    Dinv = sp.diags(dgl.backend.asnumpy(g.in_degrees()).clip(1) ** -1.0, dtype=float) # D^-1
+    RW = A @ Dinv  
+    M = RW
+    # Iterate
+    PE = [torch.from_numpy(M.diagonal()).float()]
+    M_power = M
+    for _ in range(pe_dim-1):
+        M_power = M_power @ M
+        PE.append(torch.from_numpy(M_power.diagonal()).float())
     PE = torch.stack(PE,dim=-1)
     g.ndata['pe'] = PE  
+
+    # k-step PageRank features
+    # A = g.adjacency_matrix(scipy_fmt="csr")
+    # D = A.sum(axis=1) # out degree
+    # Dinv = 1./ (D+1e-9); Dinv[D<1e-9] = 0 # take care of nodes without outgoing edges
+    # Dinv = sp.diags(np.squeeze(np.asarray(Dinv)), dtype=float) # D^-1 
+    # P = (Dinv @ A).T 
+    # n = A.shape[0]
+    # One = np.ones([n])
+    # x = One/ n
+    # PE = [] 
+    # alpha = 0.95 
+    # for _ in range(pe_dim): 
+    #     x = alpha* P.dot(x) + (1.0-alpha)/n* One 
+    #     PE.append(torch.from_numpy(x).float())
+    # PE = torch.stack(PE,dim=-1)
+    # g.ndata['pe'] = PE  
 
     return g
 