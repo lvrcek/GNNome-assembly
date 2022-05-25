3e5c95d77e096fef21f2b9c1823237923dd4ec33
Lovro Vrcek <vrcek.lovro@gmail.com>
2022-05-25 12:54:01 +0800

    Delete comments

--- evaluate.py
+++ evaluate.py
@@ -40,11 +40,6 @@ def walk_to_sequence(walks, graph, reads, edges):
         prefixes = [(src, graph.edata['prefix_length'][edges[src,dst]]) for src, dst in zip(walk[:-1], walk[1:])]
         sequences = [reads[src][:prefix] for (src, prefix) in prefixes]
         sequence = ''.join(map(str, sequences)) + reads[walk[-1]]
-        # for src, dst in zip(walk[:-1], walk[1:]):
-        #     edge_id = edges[(src, dst)]
-        #     prefix = graph.edata['prefix_length'][edge_id].item()
-        #     sequence += reads[src][:prefix]
-        # sequence += reads[walk[-1]]
         sequence = SeqIO.SeqRecord(sequence)
         sequence.id = f'contig_{i+1}'
         sequence.description = f'length={len(sequence)}'
